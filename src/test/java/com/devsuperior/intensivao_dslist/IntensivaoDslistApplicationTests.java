b7c6fe05753af1d2d6af1809d828d8ea71938880
engnogueira <engnogueira@hotmail.com>
2025-01-23 00:18:29 -0300

    Iniciando projeto - Java/Spring

---
+++ src/test/java/com/devsuperior/intensivao_dslist/IntensivaoDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.intensivao_dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivaoDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
